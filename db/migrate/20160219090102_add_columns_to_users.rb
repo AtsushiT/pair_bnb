e12c2f6664a547d0110b0ae8f0d2e8a47f2e0ee3
Atsushi Toyokura <atsushi.toyokura@gmail.com>
2016-02-19 19:52:28 +0800

    devise install for user management

---
+++ db/migrate/20160219090102_add_columns_to_users.rb
@@ -0,0 +1,9 @@
+class AddColumnsToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :gender, :string
+    add_column :users, :phone_number, :string
+    add_column :users, :address, :text
+  end
+end
